62fb94711ec51101ddbbc1a2bfdb1e9863bfa19a
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-10-22 12:27:03 +0600

    add jwt token

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
-
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 9000
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  optionalSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
@@ -23,12 +29,51 @@ const client = new MongoClient(uri, {
   },
 })
 
+// verify token
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token
+  if (!token) return res.status(401).send({ message: 'unauthorized access' })
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+  })
+  next();
+}
+
 async function run() {
   try {
 
     const db = client.db('solo-job')
     const jobsCollection = db.collection('jobs')
     const bidsCollection = db.collection('bids')
+
+    // generate jwt
+    app.post('/jwt', async (req, res) => {
+      const email = req.body;
+      // create jwt 
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+        .send({ success: true })
+    })
+
+    // logout || clear cookie from browser
+    app.get('/logout', async (req, res) => {
+      res.clearCookie('token', {
+        maxAge: 0,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+        .send({ success: true })
+    })
 
     // save a jobData in db
     app.post('/add-job', async (req, res) => {
